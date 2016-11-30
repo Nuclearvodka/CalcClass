66397e37918ee623bc1e732aac96df85f98e530d
Vitaly Volozhinov <radioactive4touch@gmail.com>
2016-11-30 11:47:22 +0100

    Different Options for the Calc Class Have Been Set

--- CalcClass/CalcClass.cpp
+++ CalcClass/CalcClass.cpp
@@ -1,11 +1,22 @@
-// CalcClass.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-
+//Calculator program
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+	int menu; //Variable for the number the user inputs
+	
+	printf("Enter a number from the list below\n\n");
+
+	printf("1. Addition\n"); //User Must Select an Option for the function to be performed in the Menu 
+	printf("2. Subtraction\n");
+	printf("3. Multiplication\n");
+	printf("4. Division\n\n");
+
+	printf("Enter number: "); //User input for the calculator menu
+	scanf("%d", &menu);
+
+	printf("\n");
     return 0;
 }
 
